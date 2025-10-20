b44dc2a4797bac50e294466b166c78d5ab9d80c7
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2025-10-20 19:58:01 -0300

    inicializando

---
+++ main.py
@@ -0,0 +1,3 @@
+# inicializando o repositório apenas, nada feito ainda
+# adendo: baixar a base de dados localmente. não é uma boa prática ter uma base de dados no repositório, então não coloquei no github
+# ^^(o .gitignore ignora arquivo .csv)
